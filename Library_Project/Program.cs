445d79fc6d94c4255cbbfb0649d0f57b42230614
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-12-10 23:53:57 +0300

    Add files via upload

--- Library_Project/Program.cs
+++ Library_Project/Program.cs
@@ -6,13 +6,14 @@ using System.Windows.Forms;
 
 namespace Library_Project
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
+        
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
--- Library_Project/frmLogin.cs
+++ Library_Project/frmLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,14 +18,33 @@ namespace Library_Project
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            using (SqlConnection connection = SqlCon.Connect())
+            {
+                connection.Open();
+                string queryforLogin = "Select * from AppUsers where UserName = @username and Password = @password";
 
+                using (SqlCommand cmd = new SqlCommand(queryforLogin, connection))
+                {
+                    cmd.Parameters.AddWithValue("@username", tbxUsername.Text);
+                    cmd.Parameters.AddWithValue("@password", tbxPassword.Text);
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            MessageBox.Show("Giriş Başarılı");
+                        }
+
+                        else
+                        {
+                            MessageBox.Show("Giriş Başarısız");
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
